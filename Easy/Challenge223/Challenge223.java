c689690c0ee34ca83d154743d084cef835dfbade
RossMeikleham <rossmeikleham@hotmail.co.uk>
2015-08-22 19:03:35 +0100

    Completed Easy Challenge 223

---
+++ Easy/Challenge223/Challenge223.java
@@ -0,0 +1,18 @@
+public class Challenge223 {
+    
+    public static void main(String[] args) {
+        int maxGarland = 0;
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Garland no for " + args[i] + ": " + garland(args[i]));
+        } 
+    }
+    
+    public static int garland(String s) {
+        for (int i = s.length() - 1; i > 0; i--) {
+            if (s.substring(0, i).equals(s.substring(s.length() - i, s.length()))) {
+                return i;
+            }
+        }
+        return 0;
+    }    
+}
